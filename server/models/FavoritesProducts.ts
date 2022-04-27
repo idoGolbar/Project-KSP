bb61acd66c4574ab997932accf7c650f762f7421
Matan Swisa <matanswisa01@gmail.com>
2022-04-27 17:54:42 +0300

    fix schema type of favorite products

--- server/models/FavoritesProducts.ts
+++ server/models/FavoritesProducts.ts
@@ -1,12 +1,12 @@
-import { Schema, model, connect, Types, ObjectId } from 'mongoose';
+import { Schema, model, connect, Types, ObjectId, SchemaDefinitionProperty } from 'mongoose';
 import IFavoriteProduct from '../interfaces/FavoriteProduct';
 
 
 
 const FavoriteProductSchema = new Schema<IFavoriteProduct>({
-    user_id: { type: Types.ObjectId, required: true },
-    products_ids: [{ type: Types.ObjectId, required: true },
+    user_id: { SchemaDefinitionProperty: Types.ObjectId, required: true },
+    products_ids: [{ type: Types.ObjectId, required: true }],
 }, { timestamps: true })
 
 
-export default model<IFavoriteProduct>('NewUser', FavoriteProductSchema); 
+export default model<IFavoriteProduct>('FavoritesProducts', FavoriteProductSchema);  
