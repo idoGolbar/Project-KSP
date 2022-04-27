597a796ad3c3a2f2e94c2323b09abfb617d61c73
Matan Swisa <matanswisa01@gmail.com>
2022-04-27 16:56:42 +0300

    finish favorites product schema

--- server/models/FavoritesProducts.ts
+++ server/models/FavoritesProducts.ts
@@ -5,7 +5,7 @@ import IFavoriteProduct from '../interfaces/FavoriteProduct';
 
 const FavoriteProductSchema = new Schema<IFavoriteProduct>({
     user_id: { type: Types.ObjectId, required: true },
-    products_ids: [{ type: Types.ObjectId, required: true }],
+    products_ids: [{ type: Types.ObjectId, required: true },
 }, { timestamps: true })
 
 
