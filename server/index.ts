20bb6922e64c158f32356697a3302602417dfbe4
Matan Swisa <matanswisa01@gmail.com>
2022-05-18 18:48:23 +0300

    merged favorites products branch

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,11 @@ import { ConnectMongoOptions } from 'connect-mongo/build/main/lib/MongoStore';
 dotenv.config();
 
 
-// const connection = mongoose.createConnection(process.env.DB_URL as string);
+const connection = mongoose.connect(process.env.DB_URL as string).then(() => {
+    console.log('DB connection established');
+}).catch((err) => {
+    console.log(err);
+});
 
 
 
